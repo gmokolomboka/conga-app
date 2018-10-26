466ae54fe13e88134b1e52b74593112dcb5de77c
mokolomboka <mokolomboka@yahoo.fr>
2018-10-26 05:19:23 +0200

    Displaying isUp on Dashborad

--- conga-frontend/src/app/pages/dashboard/dashboard.module.ts
+++ conga-frontend/src/app/pages/dashboard/dashboard.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-
-
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
-
+import { IsUpComponent } from '../customers/is-up/is-up.component';
 
 @NgModule({
   imports: [
     ThemeModule,
   ],
   declarations: [
-    DashboardComponent,
+    DashboardComponent, IsUpComponent
   ],
 })
 export class DashboardModule { }
